11f37a485d59a8376f283da6b56a938ff1cc89ef
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-27 19:11:31 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,49 +8,14 @@ attr_reader
   def initialize(brand)
   @brand = brand
   end
-
-end
-
+  def coobble
+  end
+end 
 adidas = Shoe.new("adidas")
 adidas.color = "red"
 adidas.size = "9.5"
 adidas.material = "suede"
 adidas.condition = "tattered"
-
-
-
-# class Book
-
-#   attr_reader :title 
-#   attr_accessor :author, :page_count, :genre
-  
-#   def initialize(title)
-#     @title = title
-#   end 
-# def turn_page
-#   puts ("Flipping the page...wow, you read fast!")
-# end
-# end 
-
-# the_great_gatsby = Book.new("The Great Gatsby")
-
-# the_great_gatsby.author = "F Scott Fitzgerald"
-# the_great_gatsby.genre = "fiction"
-# bravenewworld = Book.new("bravenewworld")
-# puts the_great_gatsby.title
-# puts bravenewworld.title
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
