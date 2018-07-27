db5c48e8265becdb123765da85beee2cc8983a38
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-27 18:53:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,10 +5,18 @@ class Shoe
 attr_accessor :brand
 attr_reader 
 
-  def initialize
+  def initialize(brand)
   @brand = brand
   end
 end
+
+adidas = Shoe.new("adidas")
+
+
+
+
+
+
 # class Book
 
 #   attr_reader :title 
