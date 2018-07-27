8fba5384dedb1ce1e8f97a6df4ec54c7e8a02e3f
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-27 18:24:55 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,9 +2,10 @@
 # Add you Book class here
 
 class Book
-  attr_reader :title
-  def initialize(title)
+  attr_reader :title, :auther
+  def initialize(title, auther)
 @title = title
+@auther = auther
 
 end 
   end 
