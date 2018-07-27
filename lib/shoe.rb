b9dd1b45be28bf7ab66aff6b8c482ee9a3088f46
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-27 19:15:45 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,15 +10,17 @@ attr_reader
   end
   def cobble
   puts "Your shoe is as good as new!"
-  @shoe.condition = "new"
+
+  end
+  def new
+    @shoe.condition = "new"
   end
 end 
 adidas = Shoe.new("adidas")
 adidas.color = "red"
 adidas.size = "9.5"
 adidas.material = "suede"
-adidas.condition = "tattered"
-   @shoe.condition = "new"
+
    
 
 
