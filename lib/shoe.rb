45cd4a0eee680cb1f09828de9d3bb3335d05681a
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-27 19:17:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,15 +5,17 @@ class Shoe
 attr_accessor :brand, :color, :size, :material, :condition
 attr_reader 
 
-  def initialize(brand)
-  @brand = brand
-  end
+    def initialize(brand)
+      @brand = brand
+    end
+  
     def cobble
-    puts "Your shoe is as good as new!"
-  end
-  def new
-    @shoe.condition = "new"
-  end
+      puts "Your shoe is as good as new!"
+    end
+    
+    def new
+      @shoe.condition = "new"
+    end
 end 
 adidas = Shoe.new("adidas")
 adidas.color = "red"
