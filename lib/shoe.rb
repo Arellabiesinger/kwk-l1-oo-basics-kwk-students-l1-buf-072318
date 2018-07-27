aab964e07c3fbc37b17503df1658dad65c511b45
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-27 19:16:46 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,9 +8,8 @@ attr_reader
   def initialize(brand)
   @brand = brand
   end
-  def cobble
-  puts "Your shoe is as good as new!"
-
+    def cobble
+    puts "Your shoe is as good as new!"
   end
   def new
     @shoe.condition = "new"
