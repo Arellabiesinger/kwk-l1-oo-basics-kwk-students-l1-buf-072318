277fe7e7575e0dc9056f55445eebab79e6dbdc9c
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-27 18:52:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,12 +8,27 @@ attr_reader
   def initialize
   @brand = brand
   end
-
-
-
-
 end
+# class Book
 
+#   attr_reader :title 
+#   attr_accessor :author, :page_count, :genre
+  
+#   def initialize(title)
+#     @title = title
+#   end 
+# def turn_page
+#   puts ("Flipping the page...wow, you read fast!")
+# end
+# end 
+
+# the_great_gatsby = Book.new("The Great Gatsby")
+
+# the_great_gatsby.author = "F Scott Fitzgerald"
+# the_great_gatsby.genre = "fiction"
+# bravenewworld = Book.new("bravenewworld")
+# puts the_great_gatsby.title
+# puts bravenewworld.title
 
 
 
